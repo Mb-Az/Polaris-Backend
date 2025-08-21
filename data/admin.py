74ae571627ffd681dc29ea8cc0502a0de8bbe2cf
Mobin Azadani <azad.1214mobin@gmail.com>
2025-08-21 13:44:35 +0330

    admin panel access to models

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TestResult, CellMeasurement
+admin.site.register(TestResult)
+admin.site.register(CellMeasurement)
